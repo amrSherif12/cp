51b74f4ecd044a1e1bea8fc87c2a8ff85aa0ba96
amrSherif12 <105169718+amrSherif12@users.noreply.github.com>
2026-02-02 14:04:12 +0200

    Implement Edge struct for weighted graph
    Added Edge struct for weighted graph with comparison operator.

--- graph.cpp
+++ graph.cpp
@@ -1,1 +1,11 @@
 // In a complete graph (clique) with V vertices, the number of edges E must be exactly: V * (V - 1) / 2
+
+
+// when making an edge struct for weighted graph u can define an opperator to be able to use the struct in pq
+struct Edge {
+    int u, v, c;
+
+    bool operator>(const Edge &other) const {
+        return c > other.c;
+    }
+};
