1eadcd344c57c6a9aa721e7ed90781ab0c1a664d
amrSherif12 <105169718+amrSherif12@users.noreply.github.com>
2026-02-02 14:41:48 +0200

    Add function for calculating 2nd MST
    Implemented a function to calculate the second minimum spanning tree (MST) using a priority queue.

--- graph.cpp
+++ graph.cpp
@@ -9,3 +9,30 @@ struct Edge {
         return c > other.c;
     }
 };
+
+
+// function for getting 2nd MST
+
+int mst(int n, map<int, vii> &adj, int bu, int bv, vector<Edge> &edges, bool sve) {
+    int tot = 0;
+    vb vis(n + 1, false);
+    int edg = 0;
+    priority_queue<Edge, vector<Edge>, greater<> > pq;
+    pq.emplace(-1, 1, 0);
+    while (!pq.empty()) {
+        auto [u, v, c] = pq.top();
+        pq.pop();
+        if (vis[v]) continue;
+        vis[v] = true;
+        tot += c;
+        edg++;
+        if (sve && u != -1) edges.emplace_back(u, v, c);
+        for (auto [nc, na]: adj[v]) {
+            if ((v == bu && na == bv) || (na == bu && v == bv)) continue;
+            if (!vis[na]) {
+                pq.emplace(v, na, nc);
+            }
+        }
+    }
+    return edg == n ? tot : INT_MAX;
+}
