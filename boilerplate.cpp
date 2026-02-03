f18ccd3c4775723f91dadd84dd454f2f90a2d478
amrSherif12 <105169718+amrSherif12@users.noreply.github.com>
2026-02-03 12:08:57 +0200

    Refactor boilerplate.cpp with new includes and macros
    Updated includes and added new type definitions.

--- boilerplate.cpp
+++ boilerplate.cpp
@@ -1,28 +1,31 @@
-
-#include <iostream>
-#include <vector>
 #include <algorithm>
-#include <cmath>
+#include <charconv>
+#include <complex>
 #include <iomanip>
-#include <map>
+#include<iostream>
+#include<map>
+#include <numeric>
+#include <queue>
 #include <set>
+#include <unordered_set>
+#include <vector>
+#include <climits>
 using namespace std;
- 
+
 #define ll long long
-#define ld long double
-#define ull unsigned long long
-#define pii pair<int,int>
-#define pll pair<long long,long long>
+#define dl long double
 #define vi vector<int>
-#define vll vector<long long>
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define pb push_back
-#define ff first
-#define ss second
- 
-#define rep(i,a,b) for (int i = (a); i < (b); i++)
-#define rrep(i,a,b) for (int i = (a); i >= (b); i--)
-#define each(x,a) for (auto &x : a)
- 
+#define vb vector<bool>
+#define vd vector<double>
+#define vll vector<ll>
+#define vii vector<pii>
+#define vdd vector<pdd>
+#define si set<int>
+#define sdd set<pdd>
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define pll pair<ll, ll>
+#define mii map<int, int>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define lp(i, a, b) for (int i = (a); i < (b); i++)
 #define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
