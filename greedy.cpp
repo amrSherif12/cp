749dae249427cb5560e79a07c2ac159bba9d97e5
amrSherif12 <105169718+amrSherif12@users.noreply.github.com>
2026-02-05 12:09:14 +0200

    Update greedy.cpp

--- greedy.cpp
+++ greedy.cpp
@@ -1,5 +1,5 @@
 // kaden
-
+// for finding the finding the largest sum in an array with nega numbers
 int maxSubArray(vector<int>& nums) {
         int mx = INT_MIN, cur = 0;
         for (int x : nums) {
@@ -8,4 +8,23 @@ int maxSubArray(vector<int>& nums) {
             if (cur < 0) cur = 0;
         }
         return mx;
+    }
+
+
+
+// patience sort
+// for finding lis
+
+int lengthOfLIS(vector<int>& nums) {
+        vector<int> ans;
+        ans.push_back(nums[0]);
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > ans.back()) {
+                ans.push_back(nums[i]);
+            } else {
+                int in = lower_bound(ans.begin(), ans.end(), nums[i]) - ans.begin();
+                ans[in] = nums[i];
+            }
+        }
+        return ans.size();
     }
