98c4e67ffd3b2c9d4f7fff788706cc8e2c38b108
amrSherif12 <105169718+amrSherif12@users.noreply.github.com>
2026-02-12 11:12:43 +0200

    Update set.cpp with ordered multiset definition
    Refactor ordered set definition and add ordered multiset.

--- set.cpp
+++ set.cpp
@@ -3,14 +3,14 @@
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
- 
-typedef tree<
-    int,
-    null_type,
-    less<int>,
-    rb_tree_tag,
-    tree_order_statistics_node_update>
-
-ordered_set;
+typedef tree<int, null_type,less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 
 s.order_of_key(v);
+
+
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less_equal<ll>, rb_tree_tag,tree_order_statistics_node_update> ordered_multiset;
+ 
