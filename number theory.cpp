d2472d93bd721b56f0e25eef9b6479087f058dda
amrSherif12 <105169718+amrSherif12@users.noreply.github.com>
2026-04-10 15:53:54 +0200

    Update number theory.cpp

--- number theory.cpp
+++ number theory.cpp
@@ -1,3 +1,11 @@
+// when doing an exponential its better use bit shiffting for percision
+cout << pow(2, res); // bad
+cout << (1LL << res); // good
+// gcd(a, b) = gcd(a % b, b)
+// gcd(0, x) = x
+// (a + b) % m = ((a % m) + (b % m)) % m
+
+
 // counting number of divisors in O(N logN)
 
 const int MAXN = 1000001;
@@ -114,3 +122,43 @@ std::vector<long long> getPrimeFactors(long long n) {
 
     return factors;
 }
+
+
+
+
+
+ /*
+ oprimized power function with mod
+ Computes (base^exp) % mod
+ * Time Complexity: O(log exp)
+ * Space Complexity: O(1)
+ */
+long long binpow(long long base, long long exp, long long mod = 1e9 + 7) {
+    long long res = 1;
+    base %= mod;
+    while (exp > 0) {
+        if (exp & 1) res = (__int128)res * base % mod;
+        base = (__int128)base * base % mod;
+        exp >>= 1;
+    }
+    return res;
+}
+
+
+// without mod
+long long binpow_raw(long long base, long long exp) {
+    long long res = 1;
+    while (exp > 0) {
+        if (exp & 1) res = res * base;
+        base = base * base;
+        exp >>= 1;
+    }
+    return res;
+}
+
+
+
+long long ceil(long  long a, long long b) {
+ return (a + b - 1) / b;
+}
+ 
